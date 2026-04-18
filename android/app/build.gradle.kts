50ca06d40a99dd61ea329846b0737eb716398af6
bangunaji <alfahriaji9@gmail.com>
2026-04-18 15:21:26 +0700

    Fix build: Increased minSdkVersion to 23 for Firebase compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.pixvault"
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
